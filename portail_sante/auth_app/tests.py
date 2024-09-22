1f0f892316c15c3aac65f9cd8f94418d2829f948
yvanoide <iffikig71@gmail.com>
2024-09-22 20:58:35 +0200

    Ajout de la CI/CD

--- portail_sante/auth_app/tests.py
+++ portail_sante/auth_app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SimpleTestCase(TestCase):
+    def test_homepage_status_code(self):
+        # Suppose the homepage is at '/'
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
